fb4d4ce73d968b63b83365ea72075d9684f2c9da
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-07-17 17:15:01 -0500

    Extensión carrito
    Un pequeño desarrollo para lo que corresponde a agregar y borrar articulos del carrito

--- Front/assets/dist/js/Carrito.js
+++ Front/assets/dist/js/Carrito.js
@@ -1,10 +1,31 @@
-class Carrito{
-    constructor() {}
-
-    //Métodos
-    agregarCarrito(){}
-    eliminarCarrito(){}
-    procedePago(){}
-    cancelaPago(){}
-    
+let Productos={}
+let Respuesta = {
+    codigo: 200,
+    error: false,
+    mensaje: ''
 }
+
+class Product{
+    constructor(nombre, precio,id) {
+        this.nombre=nombre
+        this.precio=precio
+        this.id=id
+    } 
+}
+
+const newProduct = agregarCarrito= (nombre, precio, id) => {
+    Productos[nombre] = new Product (nombre, precio,id)
+
+}
+
+const delProduct = eliminarCarrito = (nombre) => {
+    if(Productos.hasOwnProperty(nombre)){
+        delete Productos[nombre]
+        return true
+    }else{
+        return false
+    }
+}
+
+procedePago = () => {}
+cancelaPago = () => {}
