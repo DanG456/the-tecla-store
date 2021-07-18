28750bc1e266eb1a81f3afbb87e3f9fe9d810cf3
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-07-18 12:59:07 -0500

    Actualización de las rutas
    Se agregaron las de Registro, Log in, se realizaron unas cuantas modificaciones a las otras y se añadieron un par de comentarios

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,47 @@
 //Importar modulos necesarios
+const db=require('../db/db');
 const chkAcc = require("../Middleware/midd")
 const Carrito = require ("../assets/dist/js/Carrito")
-const cors = requiere ('cors')
+const cors = require ('cors')
 //Exportar las rutas para trabajar el server
 
 module.exports = (app) =>{
-    //Iniciamos el endpoint
+    //Iniciamos los endpoint
+
+    app.get('/index',(req,res) => {
+        //Respuesta de inicio correcto
+        db.Respuesta={
+            codigo: 200,
+            error: false,
+            mensaje: "Bienvenido a The Tecla Store"
+        }
+        res.send(db.Respuesta)
+    })
+
+    app.get('/login_demo',(req,res) => {
+        db.Respuesta={
+            codigo:200,
+            error: false,
+            mensaje: "Página para log in del usuario"
+        }
+        res.send(db.Respuesta)
+    })
+
+    app.get('/Registro',(req,res) => {
+        db.Respuesta={
+            codigo:200,
+            error: false,
+            mensaje: "Usuario cree una cuenta para empezar su experiencia con nosotros"
+        }
+        res.send(db.Respuesta)
+    })
+
+    //Devuelve los datos de los productos en la BD
+    app.get('/products',(req,res)=>{
+        res.send(db.Productos)
+    })
+
+    //Informa que se añadio un producto al carrito
     app.post('/Pago_Arts',chkAcc.checkAccount, (req,res)=>{
         Carrito.agregarCarrito(req.body.nombre, req.body.precio, req.body.id)
 
@@ -17,8 +53,9 @@ module.exports = (app) =>{
 
         res.send(Carrito.Respuesta)
     })
-    
-     app.delete('/Pago_Arts/:nombre',chkAcc.checkAccount, (req,res)=>{
+
+    //Eliminar articulos del carrito
+    app.delete('/Pago_Arts/:nombre',chkAcc.checkAccount, (req,res)=>{
         if(Carrito.eliminarCarrito(req.params.nombre)){
             Carrito.Respuesta={
                 codigo:200,
