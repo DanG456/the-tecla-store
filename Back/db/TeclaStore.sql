b19ae92e42a1150ffaf640522de616e10759fb7c
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-07-25 13:38:56 -0500

    archivo de la Base de datos

---
+++ Back/db/TeclaStore.sql
@@ -0,0 +1,46 @@
+IF NOT EXISTS(
+	SELECT name
+	FROM sys.databases
+	WHERE name= N'TeclaStore'
+)
+
+CREATE DATABASE TeclaStore
+GO
+
+CREATE TABLE usuarios(
+	iduser int NOT NULL IDENTITY (1,1),
+	nombre char(100) NOT NULL,
+	apellidos char(100) NOT NULL,
+	contraseña varchar(20) NOT NULL,
+	PRIMARY KEY (iduser)
+)
+GO
+
+CREATE TABLE productos(
+	idprod int NOT NULL,
+	nombreprod varchar(100) NOT NULL,
+	precio float NOT NULL,
+	categoria char(50) NOT NULL,
+	PRIMARY KEY (idprod)
+)
+GO
+
+CREATE TABLE carrito(
+	iduser int NOT NULL,
+	idprod int NOT NULL,
+	cantidad int NOT NULL,
+	precioT int NOT NULL,
+	FOREIGN KEY (iduser) REFERENCES usuarios(iduser),
+	FOREIGN KEY (idprod) REFERENCES productos(idprod)
+)
+GO
+
+INSERT INTO usuarios (nombre, apellidos, contraseña)
+	VALUES ('Daniel Arturo', 'Gil Quezada', 'admin1')
+GO
+
+INSERT INTO usuarios (nombre, apellidos, contraseña)
+	VALUES ('Mauricio', 'Aviles Piña', 'admin2')
+GO
+
+SELECT * FROM usuarios
