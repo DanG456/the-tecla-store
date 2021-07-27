d8f8ac508bf40de4d967940fbda0a77e4c53b088
mauavilespi <mauavilespi@outlook.com>
2021-07-26 19:18:22 -0500

    Conexión base de datos MMSQL

--- Back/db/connection.js
+++ Back/db/connection.js
@@ -1,19 +1,11 @@
 const Sequelize = require('sequelize')
+//*Dotenv
+require('dotenv').config();
 
-const sequelize  = new Sequelize('TeclaStore', null, null, {
+const sequelize  = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   dialect: 'mssql',
-  server: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialectOptions: {
-    authentication: {
-      type: 'default',
-      options: {
-        encrypt: true,
-        userName: process.env.DB_USER,
-        password: process.env.DB_PASS
-      }
-    }
-  }
 })
 
 module.exports = sequelize
