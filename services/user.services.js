b2874bda2770fe56682143ea5fa75ba110a6682e
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-07-30 13:27:31 -0500

    Create user.services.js

---
+++ services/user.services.js
@@ -0,0 +1,30 @@
+const sequelize=require('../db/connection')
+
+module.exports.createUser=async (user) =>{
+	let newUser=[
+		user.iduser,
+		user.nombre,
+		user.apellidos,
+		user.contrasenia
+	]
+	console.log(newUser)
+  try {
+    let resultado = await sequelize.query(`INSERT INTO usuarios (iduser, nombre, apellidos, contraseña) VALUES (?, ?, ?, ?)`,
+    {replacements: newUser, type: sequelize.QueryTypes.SELECT});
+    console.log(resultado)
+    return resultado
+  } catch(err) {
+    console.log(err)
+    throw new Error('Ocurrio un error en la creación de usuario')
+  }
+}
+
+module.exports.showUsers=async ()=>{
+  	try{
+  		let resultado = await sequelize.query('SELECT * FROM usuarios')
+    	return resultado
+  	}catch(err){
+  		console.log(error)
+    	throw new Error ('Ocurrio un error en la consulta de usuarios')
+  	}
+}
