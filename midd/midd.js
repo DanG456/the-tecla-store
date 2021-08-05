bcef35c2ab183269c0bdc3a4f5be30fe0cfb6afc
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-08-04 19:32:27 -0500

    Update midd.js

--- midd/midd.js
+++ midd/midd.js
@@ -1,14 +1,30 @@
 //?Importamos los módulos necesarios
 //*Express Rate Limit
 const rateLimit = require('express-rate-limit');
+const userController=require('../app/controlador/controlador.usuarios')
 
 //?Middlewares
 //*Limitar el uso de la API
-const theLimit = rateLimit({
+module.exports.theLimit = rateLimit({
     windowMs: 30 * 60 * 1000, //* 30 minutos de uso
     max: 100, //* 10 peticiones por ventana
     message: "Usted ha excedido los límites de acceso a la API"
 });
 
-//?Exportamos las funciones
-module.exports = {theLimit}
+module.exports.validUser = async (req,res,next)=>{
+    try {
+        if (req.headers.authorization != undefined){
+            const token = req.headers.authorization.split(' ')[1]
+            let verificado = await userController.verifyUser(token)
+            console.log(verificado)
+            req.params.usuario = verificado.data
+            return next()
+        }else{
+            throw new Error ('Este es un sistema seguro y requiere autorización')
+        }
+    }catch (err){
+        console.log(err.message)
+        res.status(500).json({error: err.message})
+    }
+}
+
