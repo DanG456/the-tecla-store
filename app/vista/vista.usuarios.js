7b2c1b520b9d49d550474a9ff6f0487b6719aff8
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-08-08 12:43:31 -0500

    Update vista.usuarios.js

--- app/vista/vista.usuarios.js
+++ app/vista/vista.usuarios.js
@@ -6,7 +6,22 @@ const midd = require('../../midd/midd');
 //Exportamos los modulos a ser utilizados
 
 //Metodos GET
-
+module.exports = async (app) => {
+    //? Raise our server
+    app.listen(process.env.PORT, () => {
+        console.log(`Servidor iniciado en http://${process.env.HOST}:${process.env.PORT}`);
+     });
+    
+    app.get('/usuarios', async(req,res)=>{
+        try {
+            let resultado = await controllerUsers.showUsers()
+            res.status(200).json({ message: "Datos recuperados exitosamente", resultado})
+        } catch (err) {
+            console.log(err.message)
+            res.status(500).json({ message: "Error en el servidor", error: err.message})
+        }
+    })
+}
 //Metodos POST
 module.exports = async (app) =>{
     app.post('/login',async(req,res)=>{
@@ -44,15 +59,35 @@ module.exports = async (app) =>{
 
 //Metodos UPDATE
 module.exports=async(app)=>{
-    app.post('/usuario/:nombre',async(req,res)=> {
+    app.patch('/usuario/:nombre',async(req,res)=> {
+        let newName = req.body
+        let currName = req.params.nombre
+        try{
+            let usr = controllerProducts.updateUser(newName,currName)
+            return usr
+        }catch(error){
+            console.log(error)
+            res.status(400).send('Ocurrio un error en la actualización del usuario')
+        }
+    })
 
+    app.patch('/categorias/:categoria',async(req,res)=> {
+        let newCategory = req.body
+        let currCategory = req.params.categoria
+
+        try{
+            let cat = controllerProducts.updateCategory(newCategory,currCategory)
+        }catch(error){
+            console.log(error)
+            res.status(400).send('Ocurrrio un error en la actualización de la categoría')
+        }
     })
 }
 
 //Metodos DELETE
 module.exports=async(app)=>{
     app.delete('/eliminar/:usuario',midd.validUser(),async(req,res)=>{
-        let usr=[req.params.name, req.params.apellidos]
+        let usr=[req.params.nombre, req.params.apellidos]
         if(controllerUsers.deleteUser(usr)){
             res.status(200).json('El usuario se elimino correctamente')
         }else{
